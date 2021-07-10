bfd8bac6a561bfb894fe8c9abb27de9814123cd7
mrava87 <matteoravasi@gmail.com>
2021-07-10 15:16:45 +0300

    Updated documentation

--- docs/_downloads/55b787b46b1185dc073d3a654e3bc1aa/raymckimaging.py
+++ docs/_downloads/55b787b46b1185dc073d3a654e3bc1aa/raymckimaging.py
@@ -1,14 +1,9 @@
 """
-6. Imaging with Marchenko fields
-================================
-This example shows how to perform target-oriented imaging via the
-documentation and shows how to set-up and run the
-:py:class:`pymarchenko.imaging.MarchenkoImaging` routine. Whilst this is a
-wrapper function that can use any of the implemented Marchenko redatuming
-schemes we will use here :py:class:`pylops.waveeqprocessing.Marchenko`. For
-each depth level, up- and down-going fields will be retrieved and used to
-perform MDD. Finally both zero-offset, zero-time images and angle gathers
-are produced.
+7. Imaging with Rayleigh-Marchenko fields
+==========================================
+This example shows how to perform target-oriented imaging with
+Rayleigh-Marchenko wavefields using the
+:py:class:`pymarchenko.imaging.MarchenkoImaging` routine.
 
 """
 # sphinx_gallery_thumbnail_number = 3
@@ -18,6 +13,7 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
+from scipy.signal import convolve
 from pymarchenko.imaging import MarchenkoImaging
 
 warnings.filterwarnings('ignore')
@@ -30,13 +26,15 @@ os.environ["MKL_NUM_THREADS"] = "1"
 # Let's start by defining some input parameters and loading the test data
 
 # Input parameters
-inputfile = '../testdata/marchenko/input.npz'
+inputfile = '../testdata/raymarchenko/input.npz'
 
 vel = 2400.0         # velocity
-toff = 0.045         # direct arrival time shift
+tsoff = 0.06         # direct arrival time shift source side
+troff = 0.06         # direct arrival time shift receiver side
 nsmooth = 10         # time window smoothing
 nfmax = 500          # max frequency for MDC (#samples)
-niter = 10           # iterations
+niter = 30           # iterations
+convolvedata = True  # apply convolution to data
 igaths = [21, 31,
           41, 51,
           61]        # indeces of angle gathers
@@ -55,7 +53,7 @@ ns = s.shape[1]
 ds = s[0, 1]-s[0, 0]
 
 # Imaging domain
-nvsx, nvsz = 81, 80
+nvsx, nvsz = 81, 40
 dvsx, dvsz = 20, 10
 vsx = np.arange(nvsx)*dvsx + 700
 vsz = np.arange(nvsz)*dvsz + 650
@@ -65,15 +63,24 @@ VSX, VSZ = np.meshgrid(vsx, vsz, indexing='ij')
 rho = inputdata['rho']
 z, x = inputdata['z'], inputdata['x']
 
-# Reflection data (R[s, r, t]) and subsurface fields
-R = inputdata['R'][:, :, :-100]
-R = np.swapaxes(R, 0, 1) # just because of how the data was saved
+# Time axis
+t = inputdata['t']
+ot, dt, nt = t[0], t[1]-t[0], len(t)
 
+# Wavelet
 wav = inputdata['wav']
 wav_c = np.argmax(wav)
 
-t = inputdata['t'][:-100]
-ot, dt, nt = t[0], t[1]-t[0], len(t)
+# Reflection data (R[s, r, t]) and subsurface fields
+Vzu = inputdata['Vzu']
+Vzd = inputdata['Vzd']
+
+# Convolve data with wavelet
+if convolvedata:
+    Vzu = dt * np.apply_along_axis(convolve, -1, Vzu, wav, mode='full')
+    Vzu = Vzu[..., wav_c:][..., :nt]
+    Vzd = dt * np.apply_along_axis(convolve, -1, Vzd, wav, mode='full')
+    Vzd = Vzd[..., wav_c:][..., :nt]
 
 plt.figure(figsize=(18,9))
 plt.imshow(rho, cmap='gray', extent=(x[0], x[-1], z[-1], z[0]))
@@ -87,20 +94,21 @@ plt.title('Model and Geometry')
 plt.xlim(x[0], x[-1])
 plt.tight_layout()
 
+
 ##############################################################################
 # Let's perform imaging now
 iss, imck, ass, amck = \
     MarchenkoImaging(vsx, vsz, r, s, dr, dt, nt, vel,
-                     toff, nsmooth, wav, wav_c, nfmax, igaths, nalpha, 1,
-                     dict(R=R), kind='mck', niter=10, nproc=4)
+                     tsoff, nsmooth, wav, wav_c, nfmax, igaths, nalpha, 1,
+                     dict(Vzu=Vzu, Vzd=Vzd), kind='rmck', niter=niter, nproc=4)
 
 ##############################################################################
 # Finally let's display the images
 
 fig, axs = plt.subplots(1, 2, figsize=(17, 6))
-axs[0].imshow(iss, cmap='gray', vmin=-1e3, vmax=1e3, interpolation='sinc')
+axs[0].imshow(iss, cmap='gray', vmin=-1e6, vmax=1e6, interpolation='sinc')
 axs[0].axis('tight')
-axs[1].imshow(imck, cmap='gray', vmin=-1e-2, vmax=1e-2, interpolation='sinc')
+axs[1].imshow(imck, cmap='gray', vmin=-5e-1, vmax=5e-1, interpolation='sinc')
 axs[1].axis('tight')
 fig.tight_layout()
 
@@ -110,12 +118,12 @@ fig.tight_layout()
 ngath = len(igaths)
 fig, axs = plt.subplots(1, 2, figsize=(20, 4))
 axs[0].imshow(ass.transpose(0, 2, 1).reshape(ngath * nalpha, nvsz).T,
-              cmap='gray', vmin=-1e6, vmax=1e6,
+              cmap='gray', vmin=-5e9, vmax=5e9,
               interpolation='sinc')
 axs[0].axis('tight')
 axs[0].set_title('Gathers RTM')
 axs[1].imshow(amck.transpose(0, 2, 1).reshape(ngath * nalpha, nvsz).T,
-              cmap='gray', vmin=-1e1, vmax=1e1,
+              cmap='gray', vmin=-5e2, vmax=5e2,
               interpolation='sinc')
 axs[1].set_title('Gathers Mck')
 axs[1].axis('tight')
